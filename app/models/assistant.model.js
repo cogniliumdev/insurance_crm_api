1a145c9b731f85f1ceaa7197f211b02821540ea4
CogniliumDev <cogniliumdev@gmail.com>
2022-10-17 18:07:09 +0500

    created relations

--- app/models/assistant.model.js
+++ app/models/assistant.model.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
-    const Assistant = sequelize.define("assistant", {
+    const Consumer = sequelize.define("consumer", {
         name: {
             type: Sequelize.STRING
         },
@@ -8,8 +8,32 @@ module.exports = (sequelize, Sequelize) => {
         },
         email: {
             type: Sequelize.STRING
+        },
+        last_contact:{
+            type: Sequelize.STRING
+        },
+        tracking_data:{
+            type: Sequelize.STRING
+        },
+        brand:{
+            type: Sequelize.STRING
+        },
+        lead_type:{
+            type: Sequelize.STRING
+        },
+        referrer:{
+            type: Sequelize.STRING
+        },
+        source:{
+            type: Sequelize.STRING
+        },
+        ip_address:{
+            type: Sequelize.STRING
+        },
+        quoter_url:{
+            type: Sequelize.STRING
         }
     });
-    return Assistant;
+    return Consumer;
 }
 
